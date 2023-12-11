4c7a8c12992ef1fb87a50db8e4c87a63b52b56fe
Sam Lee <cable8mm@gmail.com>
2023-12-11 21:02:03 +1300

    Make scrums identifiable

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -1,33 +1,11 @@
-//
-//  ScrumdingerApp.swift
-//  Scrumdinger
-//
-//  Created by samgu.lee on 6/12/23.
-//
-
-import SwiftData
 import SwiftUI
+
 
 @main
 struct ScrumdingerApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        }
-        catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
-            MeetingView()
+            ScrumsView(scrums: DailyScrum.sampleData)
         }
-        .modelContainer(sharedModelContainer)
     }
 }
