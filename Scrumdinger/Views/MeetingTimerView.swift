ce9c5c36d0f3a9ea8546d3d0184efa3766e08c54
Samgu Lee <cable8mm@gmail.com>
2023-12-13 00:36:11 +1300

    Transcribing speech to text (#14)
    * Request authorization to device hardware
    
    * Integrate speech recognition
    
    * Display recording indicators
    
    * Create a history view
    
    * Display a history view
    
    * Formatting

--- Scrumdinger/Views/MeetingTimerView.swift
+++ Scrumdinger/Views/MeetingTimerView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MeetingTimerView: View {
     let speakers: [ScrumTimer.Speaker]
+    let isRecording: Bool
     let theme: Theme
 
     private var currentSpeaker: String {
@@ -20,8 +21,13 @@ struct MeetingTimerView: View {
             .strokeBorder(lineWidth: 24)
             .overlay {
                 VStack {
-                    Text("Placeholder")
+                    Text(currentSpeaker)
                         .font(.title)
+                    Text("is speaking")
+                    Image(systemName: isRecording ? "mic" : "mic.slash")
+                        .font(.title)
+                        .padding(.top)
+                        .accessibilityLabel(isRecording ? "with transcription" : "without transcription")
                 }
                 .accessibilityElement(children: .combine)
                 .foregroundStyle(theme.accentColor)
@@ -45,6 +51,6 @@ struct MeetingTimerView_Previews: PreviewProvider {
     }
 
     static var previews: some View {
-        MeetingTimerView(speakers: speakers, theme: .yellow)
+        MeetingTimerView(speakers: speakers, isRecording: true, theme: .yellow)
     }
 }
