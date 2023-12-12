c65dd93bd52796efc0878f31045dc4c726b9b352
Sam Lee <cable8mm@gmail.com>
2023-12-12 23:49:55 +1300

    Draw the progress ring

--- Scrumdinger/Views/SpeakerArc.swift
+++ Scrumdinger/Views/SpeakerArc.swift
@@ -1,15 +1,10 @@
-//
-//  SpeakerArc.swift
-//  Scrumdinger
-//
-//  Created by samgu.lee on 12/12/23.
-//
-
 import SwiftUI
+
 
 struct SpeakerArc: Shape {
     let speakerIndex: Int
     let totalSpeakers: Int
+
 
     private var degreesPerSpeaker: Double {
         360.0 / Double(totalSpeakers)
@@ -21,10 +16,11 @@ struct SpeakerArc: Shape {
         Angle(degrees: startAngle.degrees + degreesPerSpeaker - 1.0)
     }
 
+
     func path(in rect: CGRect) -> Path {
         let diameter = min(rect.size.width, rect.size.height) - 24.0
         let radius = diameter / 2.0
-        let center = CGPoint(x: rect.minX, y: rect.midY)
+        let center = CGPoint(x: rect.midX, y: rect.midY)
         return Path { path in
             path.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
         }
