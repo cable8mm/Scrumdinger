a8cb8fe9316875d59a41eab37f62f664e12b9077
Sam Lee <cable8mm@gmail.com>
2023-12-12 23:06:32 +1300

    Add an error wrapper structure

---
+++ Scrumdinger/Models/ErrorWrapper.swift
@@ -0,0 +1,21 @@
+//
+//  ErrorWrapper.swift
+//  Scrumdinger
+//
+//  Created by samgu.lee on 12/12/23.
+//
+
+import Foundation
+
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+
+
+    init(id: UUID = UUID(), error: Error, guidance: String) {
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+}
